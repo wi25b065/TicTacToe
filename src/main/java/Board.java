7aa5a3bdd76b165116937b55bd4d592d8304df0d
Konstantin <wi25b065@technikum-wien.at>
2026-05-26 12:45:25 +0200

    User_Story_3 fertig

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -3,75 +3,61 @@ public class Board {
     private char[][] cells;
 
     public Board() {
-
         // 3x3 Spielfeld erstellen
         cells = new char[3][3];
-
         clear();
     }
 
     public boolean isCellEmpty(int x, int y) {
-
         // prüfen ob Feld leer ist
         return cells[x][y] == ' ';
     }
 
     public void place(int x, int y, char marker) {
-
         // Symbol auf Feld setzen
         cells[x][y] = marker;
     }
 
-    public boolean isFull() {
 
+    // Getter-Methode, um den Inhalt einer Zelle abzurufen.
+    // Braucht man, damit die TicTacToe-Klasse die Siegesbedingungen prüfen kann.
+    public char getCell(int x, int y) {
+        return cells[x][y];
+    }
+
+
+    public boolean isFull() {
         // prüfen ob noch freie Felder existieren
         for (int i = 0; i < 3; i++) {
-
             for (int j = 0; j < 3; j++) {
-
                 if (cells[i][j] == ' ') {
-
                     return false;
                 }
             }
         }
-
         return true;
     }
 
     public void clear() {
-
         // alle Felder zurücksetzen
         for (int i = 0; i < 3; i++) {
-
             for (int j = 0; j < 3; j++) {
-
                 cells[i][j] = ' ';
             }
         }
     }
 
     public void print() {
-
-        // User Story 2:
-        // aktuelles Spielfeld anzeigen
-
+        // Aktuelles Spielfeld anzeigen
         System.out.println("_______");
-
         for (int i = 0; i < 3; i++) {
-
             System.out.print("|");
-
             for (int j = 0; j < 3; j++) {
-
                 System.out.print(cells[i][j] + "|");
             }
-
             System.out.println();
         }
-
         System.out.println("_______");
-
         System.out.println();
     }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,13 +3,11 @@ public class Player {
     private char marker;
 
     public Player(char marker) {
-
         // Spieler bekommt sein Symbol
         this.marker = marker;
     }
 
     public char getMarker() {
-
         return marker;
     }
 }
