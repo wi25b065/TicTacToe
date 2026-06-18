d877279a40a2e31c503f969c48a1428869f690ca
carolina0815 <wi25b159@technikum-wien.at>
2026-06-18 15:54:49 +0200

    Add tests for winner detection in TicTacToe

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -11,6 +11,23 @@ public class TicTacToeTest {
         game = new TicTacToe();
     }
 
+
+    @Test
+    void hasWinner_shouldReturnTrue_whenColumnIsComplete() {
+        game.board.place(0, 0, 'X');
+        game.board.place(1, 0, 'X');
+        game.board.place(2, 0, 'X');
+        assertTrue(game.hasWinner());
+    }
+
+    @Test
+    void hasWinner_shouldReturnTrue_whenDiagonalIsComplete() {
+        game.board.place(0, 0, 'X');
+        game.board.place(1, 1, 'X');
+        game.board.place(2, 2, 'X');
+        assertTrue(game.hasWinner());
+    }
+    
     // --- hasWinner ---
 
     @Test
